26cc04dae6955ef133804f088dfae662926b0fbf
saranga123 <asithasd1995@gmail.com>
2025-09-10 02:03:55 +0530

    namechange

--- server.js
+++ server.js
@@ -18,21 +18,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
+  // assign username
   socket.on('setUsername', (username) => {
     socket.username = username;
+    console.log(`User ${socket.id} set username: ${username}`);
   });
 
+  // receive messages
   socket.on('chat:message', (msg) => {
     const data = {
-      user: socket.username || 'Anonymous',
+      user: socket.username || 'Anonymous', // fallback
       text: msg,
       time: new Date().toLocaleTimeString()
     };
     io.emit('chat:message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
   });
 });
 
